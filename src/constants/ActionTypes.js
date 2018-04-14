73fdc520d561f01cdb98d432f8dc8348e63ccd36
vadim11041996 <vadim11041996@hotmail.com>
2018-04-14 16:31:19 +0300

    first commit

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,4 @@
+export const ADD_MESSAGE = "ADD_MESSAGE";
+export const MESSAGE = "MESSAGE_RECIEVED";
+export const ADD_USER = "ADD_USER";
+export const USERS_LIST = "USERS_LIST";
---
+++ src/reducers/users.js
@@ -0,0 +1,18 @@
+import * as types from '../constant/ActionTypes';
+
+const users = (state = [], action) => {
+  switch (action.type) {
+    case types.ADD_USER:
+      return state.concat([
+        {
+          name:action.name,
+          id:action.id}
+      ])
+    case  types.USERS_LIST:
+      return action.user
+    default:
+     return state;
+  }
+}
+
+export default users;
